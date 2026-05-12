3f652ede83f951f8b9f7e224984d4af9263f4aff
jonghoon.lee <greatest86@gmail.com>
2026-05-12 16:41:01 +0900

    #5.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("rendering...");
+  useEffect(() => {
+    console.log("use api");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
