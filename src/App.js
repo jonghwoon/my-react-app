1a2a06f67b20d4c06f368e330702483ec7e75e3e
jonghoon.lee <greatest86@gmail.com>
2026-05-13 11:17:59 +0900

    #7.3 coin tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [selectedPrice, setSelectedPrice] = useState(0);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        if (json.length > 0) {
+          setSelectedPrice(json[0].quotes.USD.price);
+        }
+        setLoading(false);
+      });
+  }, []);
+
+  const onAmountChange = (event) => {
+    const value = event.target.value;
+    if (value === "") {
+      setAmount(0);
+    } else {
+      setAmount(parseFloat(value));
     }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
   };
-  console.log(todos);
+
+  const onSelectChange = (event) => {
+    setSelectedPrice(parseFloat(event.target.value));
+  };
+
   return (
     <div>
-      <h1>My To Dos ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-          value={todo}
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <select onChange={onSelectChange}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <hr />
+          <div>
+            <input
+              value={amount === 0 ? "" : amount}
+              onChange={onAmountChange}
+              type="number"
+              placeholder="Enter your USD"
+            />{" "}
+            USD
+          </div>
+
+          <h3>
+            I can buy:{" "}
+            {selectedPrice > 0 ? (amount / selectedPrice).toFixed(6) : 0} units
+          </h3>
+        </>
+      )}
     </div>
   );
 }
