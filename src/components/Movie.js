31d9d046099c4f7ccada6c2b552a62d5a4884f64
jonghoon.lee <greatest86@gmail.com>
2026-05-13 15:13:20 +0900

    #7.6 Parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropsTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to={"/movie"}>{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -19,6 +19,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.PropsTypes = {
+  id: PropsTypes.number.isRequired,
   coverImg: PropsTypes.string.isRequired,
   title: PropsTypes.string.isRequired,
   summary: PropsTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://movies-api.accel.li/api/v2/movie_details.json?movie_id=${id}`,
+      )
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 
